b8516bdb8814c74e67d0846276ba287e3fbecae1
andreyosipov <Andrey.A.Osipov@nordigy.ru>
2023-09-10 12:53:11 +0400

    Add repo interface

---
+++ pharm_go_back/pharm_go_back/repository/interface.py
@@ -0,0 +1,26 @@
+import typing
+
+from pydantic import BaseModel
+
+
+class Med(BaseModel):
+    name: str
+    price: int
+    pharmacy: str
+
+
+class MedInPharmacy(BaseModel):
+    pharmacy: str
+    price: int
+
+
+class MedInfo(BaseModel):
+    name: str
+    pharmacies: list[MedInPharmacy]
+
+
+class MedsRepoProto(typing.Protocol):
+
+    async def get_med(self, med_name: str) -> MedInfo: ...
+
+    async def save_med(self, med: Med) -> None: ...
