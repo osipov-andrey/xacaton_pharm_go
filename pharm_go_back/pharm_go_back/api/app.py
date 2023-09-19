3317222387f3196879428a01d5d4cad1b9859744
glebmiller <gleb.miller.ge@gmail.com>
2023-09-19 13:49:22 +0000

    web app updated, new endpoints

--- pharm_go_back/pharm_go_back/api/app.py
+++ pharm_go_back/pharm_go_back/api/app.py
@@ -1,64 +1,114 @@
-import punq
-from quart import Quart, request, jsonify  # Use 'quart.jsonify' for Quart
+from quart import Quart, request, render_template, jsonify
+from motor.motor_asyncio import AsyncIOMotorClient
 import json
-import subprocess
-
-from quart_cors import cors
-
-from repository.interface import Med, MedsRepoProto
-from repository.local_repo import MedsRepoLocal
+from pymongo import MongoClient
 
 app = Quart(__name__)
 
+# MongoDB configuration
+MONGO_URI = "mongodb://localhost:27017"
 
-@app.route("/med", methods=["POST"])
-async def save_med():
-    repo: MedsRepoProto = container.resolve(MedsRepoProto)
-    body = await request.get_data()
-    meds = [Med(**med) for med in json.loads(body)]
-    print(meds)
-    for med in meds:
-        await repo.save_med(med)
-    return f"{len(meds)} meds have been saved", 201
+async def save_medication(data):
+    print('data =', data)
+    try:
+        async with AsyncIOMotorClient(MONGO_URI) as client:
+            db = client.medicine_database
+            collection = db.medicines
+
+            # Ensure that data is valid JSON
+            try:
+                data = json.loads(data)
+            except json.JSONDecodeError as json_error:
+                raise ValueError(f"Invalid JSON data: {str(json_error)}")
+
+            # Insert the JSON data into the "medicines" collection
+            result = await collection.insert_one(data)
+            
+            if result.inserted_id:
+                return 'Medication JSON saved successfully.', 200
+            else:
+                return 'Failed to save medication JSON.', 500
+
+    except Exception as e:
+        # Log the error for debugging purposes
+        print(f"Error while saving medication JSON: {str(e)}")
+        return 'Failed to save medication JSON.', 500
 
 
-@app.route("/med", methods=["GET"])
-async def get_meds() -> dict:
-    repo: MedsRepoProto = container.resolve(MedsRepoProto)
-    return await repo.get_meds()
+@app.route('/')
+async def index():
+    return await render_template('index.html')
 
-
-@app.route("/med/<name>", methods=["GET"])
-async def get_med(name: str) -> dict:
-    repo: MedsRepoProto = container.resolve(MedsRepoProto)
-    med_info = await repo.get_med(name)
-    if med_info:
-        return med_info.model_dump()
-    else:
-        return "No such med", 404
-
-
-@app.route("/search", methods=["POST", "GET"])
+@app.route('/search', methods=['POST'])
 async def search_medicine():
     try:
-        form_data = await request.form
-        search_query = form_data.get("query")
-        print('search_query =', search_query)
+        # Get the search query from the form
+        search_query = (await request.form)['query']
 
-        # Execute external_script.py to search for medicine
-        script_output = subprocess.check_output(["python3", "external_script.py", search_query])
-        search_results = json.loads(script_output.decode())  # Use 'decode()' to convert bytes to a string
-        print(search_results)
-        # Use 'quart.jsonify' to return JSON response
-        return jsonify(search_results)
+        # Connect to MongoDB
+        client = MongoClient(MONGO_URI)
+        db = client['medicine_database']
+
+        # Query MongoDB to find medicines with a name that matches the search query
+        results = db.medicines.find(
+            {'name': {'$regex': search_query, '$options': 'i'}},
+            {'_id': 0, 'name': 1, 'pharmacy': 1, 'price': 1}
+        )
+
+        # Convert the MongoDB cursor to a list of dictionaries
+        medicine_list = list(results)
+
+        # Close the MongoDB connection
+        client.close()
+
+        # Prepare the JSON response
+        return jsonify(medicine_list), 200
+
     except Exception as e:
-        print("error", str(e))
-        return jsonify({"error": str(e)})
+        return str(e), 500
 
 
-if __name__ == "__main__":
-    container = punq.Container()
-    container.register(MedsRepoProto, instance=MedsRepoLocal())
-    app.config["container"] = container
-    app = cors(app, allow_origin="*")
-    app.run(host="0.0.0.0", port=5005, debug=True)
+@app.route('/save', methods=['POST'])
+async def save_medication_route():
+    try:
+        data_bytes = await request.data
+
+        # Convert the bytes to a JSON string
+        data_string = data_bytes.decode('utf-8')
+
+        # Parse the JSON data
+        data = json.loads(data_string)
+
+        #data = await request.get_data()
+        print('data =', data)
+        print('datatype =', type(data))
+        for item in data:
+            # Ensure that data is valid JSON
+            #try:
+            #    data_json = json.loads(item.decode('utf-8'))
+            #except json.JSONDecodeError as json_error:
+            #    raise ValueError(f"Invalid JSON data: {str(json_error)}")
+            print(type(item))
+            print(item)
+            # Connect to MongoDB using PyMongo
+            client = MongoClient(MONGO_URI)
+            db = client['medicine_database']
+            collection = db['medicines']
+        
+            # Insert the JSON data into the "medicines" collection
+            result = collection.insert_one(item)
+            print(result)
+            client.close()
+
+            if result.inserted_id:
+                return 'Medication JSON saved successfully.', 200
+            else:
+                return 'Failed to save medication JSON.', 500
+
+    except Exception as e:
+        # Log the error for debugging purposes
+        print(f"Error while saving medication JSON: {str(e)}")
+        return 'Failed to save medication JSON.', 500
+    
+if __name__ == '__main__':
+    app.run()
